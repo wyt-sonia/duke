1eeaeff2eec29b4358e211899434bd2365e44fd5
Wang Yuting <e0365980@u.nus.edu>
2020-01-21 15:49:04 +0800

    Level 1 task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+        String stopFlag = "bye";
+        String greeting =
+                "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________";
+
+        System.out.println(greeting);
+
+        String userInput = "";
+        while(!userInput.equals(stopFlag)){
+            userInput = scanner.nextLine();
+            String reply = "";
+            if(!userInput.equals(stopFlag)) {
+                reply = "    ____________________________________________________________\n" +
+                        "     "+ userInput +"\n" +
+                        "    ____________________________________________________________\n";
+            } else {
+                reply = "    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________\n";
+            }
+            System.out.println(reply);
+        }
     }
 }
