84377f194cfed5954c5cc37655516ec62be10985
Wang Yuting <e0365980@u.nus.edu>
2020-02-11 09:23:28 +0800

    New jar file

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -65,13 +65,5 @@ public class Duke  {
         return response;
     }
 
-    /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
-     */
-    public String getResponse(String input) {
-        return "Duke heard: " + input;
-    }
-
 
 }
